f1395b45b61e0b2c0cd573d699855bead787ecbe
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-11-10 12:39:05 -0700

    Adjusted Nav Mobile And Nav Desktop [skip netlify]

--- src/components/Layouts/MainLayout/MainLayout.jsx
+++ src/components/Layouts/MainLayout/MainLayout.jsx
@@ -4,12 +4,20 @@ import { CssBaseline, Container } from "@mui/material";
 import theme from "../../theme";
 import { NavBar } from "../../NavBar";
 
+//TODO: Add pageLinks to CMS system
+const pageLinks = [
+  { link: "/", name: "home" },
+  { link: "/services", name: "services" },
+  { link: "/events", name: "events" },
+  { link: "/contact", name: "contact" },
+];
+
 const MainLayout = ({ children }) => {
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <NavBar />
+        <NavBar pageLinks={pageLinks} />
         {children}
       </ThemeProvider>
     </>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,23 +6,25 @@ import {
   useMediaQuery,
   useTheme,
   Hidden,
+  Menu,
+  MenuItem,
   Toolbar,
   Icon,
   IconButton,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
-const NavBar = () => {
+const NavBar = ({ pageLinks }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const handleMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleMenuOpen = () => {
+    setAnchorEl(true);
   };
 
   const handleMenuClose = () => {
-    setAnchorEl(null);
+    setAnchorEl(false);
   };
 
   return (
@@ -31,52 +33,77 @@ const NavBar = () => {
         <ContentContainer>
           {isMobile ? (
             <>
-              <StaticImage
-                src="../../../static/images/logo.svg"
-                alt="logo"
-                layout="fixed"
-                width={110}
-                height={100}
-                quality={80}
-              />
+              <Link to="/" label="home">
+                <StaticImage
+                  src="../../../static/images/logo.svg"
+                  alt="logo"
+                  layout="fixed"
+                  loading="eager"
+                  width={125}
+                  height={110}
+                  quality={80}
+                />
+              </Link>
               <IconButton onClick={handleMenuOpen}>
                 <MenuIcon
-                  aria-controls="mobile-menu"
+                  aria-controls="mobile-menu-open"
                   aria-haspopup="true"
                   style={{
                     color: theme.palette.text.primary,
                     height: 30,
                     width: 30,
+                    marginRight: 10,
                   }}
                 />
               </IconButton>
+              <Menu
+                id="mobile-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={anchorEl}
+                onClose={handleMenuClose}
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                sx={{ marginTop: 12, marginLeft: 1 }}
+              >
+                {pageLinks?.map((link) => (
+                  <MenuItem
+                    key={link.name}
+                    onClick={handleMenuClose}
+                    sx={{ color: theme.palette.text.primary }}
+                  >
+                    <StyledLink to={link.link}>{link.name}</StyledLink>
+                  </MenuItem>
+                ))}
+              </Menu>
             </>
           ) : (
             <>
-              <StyledLink to="/" label="home">
-                Home
-              </StyledLink>
-              <StyledLink to="/about" label="home">
-                About
-              </StyledLink>
-              <Link to="/" label="home">
-                <Hidden smDown>
+              <Hidden smDown>
+                <StyledLink to="/" label="home">
+                  Home
+                </StyledLink>
+                <StyledLink to="/about" label="home">
+                  About
+                </StyledLink>
+                <Link to="/" label="home">
                   <StaticImage
                     src="../../../static/images/logo.svg"
                     alt="logo"
                     layout="fixed"
+                    loading="eager"
                     width={150}
                     height={150}
                     quality={80}
                   />
-                </Hidden>
-              </Link>
-              <StyledLink to="/events" label="events">
-                Events
-              </StyledLink>
-              <StyledLink to="/contact" label="contact">
-                Services
-              </StyledLink>
+                </Link>
+                <StyledLink to="/events" label="events">
+                  Events
+                </StyledLink>
+                <StyledLink to="/contact" label="contact">
+                  Services
+                </StyledLink>
+              </Hidden>
             </>
           )}
         </ContentContainer>
