66a0ba49a1d8af266e15cb47df0f07343fd80bb0
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-12-20 17:33:31 -0700

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,35 @@
-import * as React from "react"
+import * as React from "react";
+import { Button, Typography, Box, Container } from "@mui/material";
+import { Seo, MainLayout } from "../components";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  )
-}
+    <>
+      <Seo title="404: Not found" />
+      <MainLayout>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            marginTop: "10rem",
+          }}
+        >
+          <Typography variant="h1">OOPS! PAGE DOESN'T EXIST</Typography>
+          <Typography variant="body1">
+            Oh no! You just hit a route that doesn&#39;t exist... the sadness. I
+            blame the developer for not making this page yet. If it SHOULD
+            exist, that is.
+          </Typography>
+          <Button variant="contained" color="primary" href="/">
+            Go Home
+          </Button>
+        </Container>
+      </MainLayout>
+    </>
+  );
+};
 
-export const Head = () => <head title="404: Not Found" />
-
-export default NotFoundPage
+export default NotFoundPage;
